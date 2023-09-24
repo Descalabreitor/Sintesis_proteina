6f6f7abe9624ff5a69e78bc983bd068288b0db46
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2023-09-24 13:12:26 +0100

    Better DNA generation

--- DNA_Generator.py
+++ DNA_Generator.py
@@ -2,7 +2,16 @@ import numpy as np
 
 class DNA_Generator:
 
-    bases = ["A", "C", "G", "T"]
+    information_codons = ["AUG", "UUU", "UUC", "UUA", "UUG", "UCU", "UCC", "UCA", "UCG",
+                     "UAU", "UAC", "UGU", "UGC", "UGG", "CUU", "CUC", "CUA", "CUG",
+                     "CCU", "CCC", "CCA", "CCG", "CAU", "CAC", "CAA", "CAG", "CGU",
+                     "CGC", "CGA", "CGG", "AUU", "AUC", "AUA", "AUG", "ACU", "ACC",
+                     "ACA", "ACG", "AAU", "AAC", "AAA", "AAG", "AGU", "AGC", "AGA",
+                     "AGG", "GUU", "GUC", "GUA", "GUG", "GCU", "GCC", "GCA", "GCG",
+                     "GAU", "GAC", "GAA", "GAG", "GGU", "GGC", "GGA", "GGG"]
+
+    start_codon = "AUG"
+    end_codons = ["UAA", "UAG", "UGA"]
 
     def __init__(self, dna_len=12):
         if dna_len % 3 != 0:
@@ -10,7 +19,8 @@ class DNA_Generator:
         self.dna_len = dna_len
 
     def gen_dna(self):
-        return ''.join(np.random.choice(self.bases) for _ in range(self.dna_len))
-
+        data =  ''.join(np.random.choice(self.information_codons) for _ in range(self.dna_len / 3))
+        return ''.join([self.start_codon, data, np.random.choice(self.end_codons)])
+    
     def gen_data(self, n_gen=10):
         return [self.gen_dna() for _ in range(n_gen)]
